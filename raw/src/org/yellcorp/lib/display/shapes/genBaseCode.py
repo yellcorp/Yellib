8edcca3e2eab75668df33b2cf312031f22933397
Jim Boswell <jimb@yellcorp.org>
2011-04-04 10:23:55 +0000

    308 simplifying genBaseCode.py

--- raw/src/org/yellcorp/lib/display/shapes/genBaseCode.py
+++ raw/src/org/yellcorp/lib/display/shapes/genBaseCode.py
@@ -1,45 +1,35 @@
-import re
-
 getset = """
-public function get ${pubname}():${proptype}  {  return ${privname};  }
-public function set ${pubname}(new_${pubname}:${proptype}):void  {  setProp("${privname}", new_${pubname});  }"""
-
-templateRE = re.compile("\\$\\{([a-z]+)\\}")
+public function get {public_name}():{var_type}  {{  return {private_name};  }}
+public function set {public_name}(new_{public_name}:{var_type}):void  {{  setProp("{private_name}", new_{public_name});  }}"""
 
 vars = """
-_width:Number
-_height:Number
-_useFill:Boolean
-_fillColor:uint
-_fillAlpha:Number
-_useLine:Boolean
-_lineThickness:Number
-_lineColor:uint
-_lineAlpha:Number
-_linePixelHinting:Boolean
-_lineScaleMode:String
-_lineCaps:String
-_lineJoints:String
-_lineMiterLimit:Number
+width:Number
+height:Number
+useFill:Boolean
+fillColor:uint
+fillAlpha:Number
+useLine:Boolean
+lineThickness:Number
+lineColor:uint
+lineAlpha:Number
+linePixelHinting:Boolean
+lineScaleMode:String
+lineCaps:String
+lineJoints:String
+lineMiterLimit:Number
 """
 
-def formatTemplate(templateString, templateExtract, varMap) :
-
-	replace = lambda m: varMap.get(m.group(1), m.group(0))
-	
-	return templateExtract.sub(replace, templateString)
-
-def formatCode(code, privateName, varType) :
-	templateVars = {
-		'privname': privateName,
-		'pubname': privateName[1:],
-		'proptype': varType
+def format_code(code, public_name, var_type) :
+	template_vars = {
+		'public_name': public_name,
+		'private_name': "_" + public_name,
+		'var_type': var_type
 	}
-	return formatTemplate(code, templateRE, templateVars)
+	return code.format(**template_vars)
 	
 
 for line in vars.split("\n") :
 	if len(line) > 0 :
 		pair = line.split(":", 1)
 		if len(pair) == 2 :
-			print(formatCode(getset, pair[0], pair[1]))
+			print(format_code(getset, pair[0], pair[1]))
